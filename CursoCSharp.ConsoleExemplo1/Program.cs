595210300f3479e0167e47cd4a97fdb8c798aca5
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2024-11-04 22:45:50 -0300

    Commit inicial

---
+++ CursoCSharp.ConsoleExemplo1/Program.cs
@@ -0,0 +1,13 @@
+﻿var nome = "Jhonatan";
+var idade = 30;
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.");
+}
+
+Console.WriteLine($"Olá, {nome}. Sua idade é {idade} anos.");
